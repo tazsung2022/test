624c0dafcbd444112de0b39ed4fe83cf8a686829
tazsung2022 <118801126+tazsung2022@users.noreply.github.com>
2023-11-14 07:24:36 +0000

    modified:   test.py

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ import streamlit as st
 st.write('hello testing')
 st.markdown('** this is a markdown')
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
