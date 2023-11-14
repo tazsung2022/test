1c631cc05ac48720a5915a85616b101e00a75a1c
tazsung2022 <118801126+tazsung2022@users.noreply.github.com>
2023-11-14 07:27:47 +0000

    modified:   test.py

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .element-container:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1) {
+                display: none;
+            }
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
